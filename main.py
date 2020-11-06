a7f9ebdb421ba532b1198903ef25415a0eb4b27c
Kenrii <you@example.com>
2020-11-06 18:45:12 +0200

    Returns json at main py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 from flask import Flask, request, render_template, json
 import collections
+import json 
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
+filename = "dump.json"
+with open(filename) as f: 
+    data = json.load(f) 
 
-
+"""@app.route('/')
+def main():
+    return render_template('index.html', data = data)"""
 @app.route('/')
 def main():
-    return render_template('index.html')
+    return data
 
 
 @app.route('/results', methods=['POST', 'GET'])
