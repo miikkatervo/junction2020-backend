f729b53fd00a5391fde75e0cac6105adf5ae888e
Miikka Tervo <miikka.tervo@gmail.com>
2020-11-07 22:06:43 +0200

    deploy

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app
+web: gunicorn app:junction2020-backend
--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,3 @@
-from app.main import app
+from junction2020-backend.main import app
 if __name__ == "__main__":
   app.run()
